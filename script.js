1687caf30d7d85b4cc85db7974624a4751f4b04b
Arjun12325144 <rajputarjun2901@gmail.com>
2024-10-05 10:27:46 +0530

    completed

---
+++ script.js
@@ -0,0 +1,27 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+
+var elem_cont = document.querySelector("#elem-cont")
+var fixed = document.querySelector("#fixed-image")
+elem_cont.addEventListener("mouseenter",function(){
+    fixed.style.display  = "block"
+})
+elem_cont.addEventListener("mouseleave",function(){
+    fixed.style.display  = "none"
+})
+
+var elems = document.querySelectorAll(".elem")
+elems.forEach(function(e){
+    e.addEventListener("mouseenter",function(){
+        var image  = e.getAttribute("data-image")
+        fixed.style.backgroundImage = `url(${image})`
+    })
+})
+
+var loader = document.querySelector("#loader")
+setTimeout(function(){
+    loader.style.top = "-100%"
+}, 4000)
